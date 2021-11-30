fc91102db89a6e3ea13ffd2142df50114ca99597
emeritus101 <88801938+emeritus101@users.noreply.github.com>
2021-11-29 20:29:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
 
 const Basic = () => (
   <div>
     <h1>Sign Up</h1>
     <Formik
       initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
+        firstName: "",
+        lastName: "",
+        email: "",
+        checked: ["Subscribe to our newsletter"]
       }}
       onSubmit={async (values) => {
         await new Promise((r) => setTimeout(r, 500));
@@ -19,10 +20,10 @@ const Basic = () => (
       <Form>
         <label htmlFor="firstName">First Name</label>
         <Field id="firstName" name="firstName" placeholder="Jane" />
-
+        <p></p>
         <label htmlFor="lastName">Last Name</label>
         <Field id="lastName" name="lastName" placeholder="Doe" />
-
+        <p></p>
         <label htmlFor="email">Email</label>
         <Field
           id="email"
@@ -30,10 +31,20 @@ const Basic = () => (
           placeholder="jane@acme.com"
           type="email"
         />
+        <p></p>
+        <label>
+          <Field
+            type="checkbox"
+            name="checked"
+            value="Subscribe to our newsletter"
+          />
+          Subscribe to our newsletter
+        </label>
+        <p></p>
         <button type="submit">Submit</button>
       </Form>
     </Formik>
   </div>
 );
 
-ReactDOM.render(<Basic />, document.getElementById('root'));
+ReactDOM.render(<Basic />, document.getElementById("root"));
